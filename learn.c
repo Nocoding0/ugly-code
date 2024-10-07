550040dae5533cfb8bd68b90fc207f9620a9763a
Nocoding0 <2609893571@qq.com>
2024-10-07 15:09:08 +0800

    Add files via upload

---
+++ learn.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    int i = 1;
+    while (i <= t)
+    {
+        printf("6");
+        i++;
+
+    }
+    
+
+    return 0;
+}
